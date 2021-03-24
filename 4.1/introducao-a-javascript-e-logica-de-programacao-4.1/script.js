473e6c6c47dfc5490f908318a86fbebb4361dd5b
Luiz Otavio <luiz.otavio273@gmail.com>
2021-03-24 18:59:38 -0300

    Exercicio 1 - operadores de adicao, subtracao, multiplicacao, dicisao e modulo

---
+++ 4.1/introducao-a-javascript-e-logica-de-programacao-4.1/script.js
@@ -0,0 +1,21 @@
+//Exercício 1
+
+let number1 = 10;
+let number2 = 8;
+
+let soma = number1 + number2;
+console.log(soma);
+
+let sub = number1 - number2;
+console.log(sub);
+
+let multi = number1 * number2;
+console.log(multi);
+
+let div = number1 / number2;
+console.log(div);
+
+let mod = number1 % number2;
+console.log(mod);
+
+
