36428588ae848e4843fc7afdd600d4b21dcdb664
Luiz Otavio <luiz.otavio273@gmail.com>
2021-03-25 01:13:14 -0300

    Exercicio 11 - Salario bruto menos impostos

--- 4.1/introducao-a-javascript-e-logica-de-programacao-4.1/script.js
+++ 4.1/introducao-a-javascript-e-logica-de-programacao-4.1/script.js
@@ -127,8 +127,76 @@ let firstNumber = 10;
 let secondNumber = 20;
 let thirdNumber = 30;
 
+allEven = false
+
 if (firstNumber % 2 === 0 && secondNumber % 2 === 0 && thirdNumber % 2 === 0) {
-  console.log('false');
-} else {
-  console.log('true');
+  allEven = true
 }
+
+console.log(allEven);
+
+//Exercício 9
+
+let firstNumber = 11;
+let secondNumber = 9;
+let thirdNumber = 42;
+
+allEven = true
+
+if (firstNumber % 2 === 0 && secondNumber % 2 === 0 && thirdNumber % 2 === 0) {
+  allEven = false
+}
+
+console.log(allEven);
+
+//Exercício 10
+
+let cost = 10;
+saleValue = 50;
+
+if (cost < 0 || saleValue < 0) {
+  console.log('Erro! Valor menor que 0');
+}
+
+totalCost = cost * 0.20 + cost;
+profit = saleValue - totalCost;
+totalProfit = profit * 1000;
+
+console.log(totalProfit);
+
+//Exercício 11
+
+let salary = 3000;
+let salaryLessINSS = null;
+let salaryLiquid = null;
+
+if (salary <= 1556.94) {
+  let taxINSS = salary * 0.08;
+  salaryLessINSS = salary - taxINSS;
+} else if (salary >= 1556.95 && salary <= 2594.92) {
+  let taxINSS = salary * 0.09;
+  salaryLessINSS = salary - taxINSS;
+} else if (salary >= 2594.93 && salary <= 5189.82) {
+  let taxINSS = salary * 0.11;
+  salaryLessINSS = salary - taxINSS;
+} else {
+  salaryLessINSS = salary - 570.88;
+}
+
+if (salaryLessINSS <= 1903.98) {
+  salaryLiquid = salaryLessINSS;
+} else if (salaryLessINSS >= 1903.99 || salaryLessINSS <= 2826.65) {
+  let taxIr = (salaryLessINSS * 0.075) - 142.80;
+  salaryLiquid = salaryLessINSS - taxIr;
+} else if (salaryLessINSS >= 2826.66 || salaryLessINSS <= 3751.05) {
+  let taxIr = (salaryLessINSS * 0.15) - 354.80;
+  salaryLiquid = salaryLessINSS - taxIr;
+} else if (salaryLessINSS >= 3751.06 || salaryLessINSS <= 4664.68) {
+  let taxIr = (salaryLessINSS * 0.225) - 636.13;
+  salaryLiquid = salaryLessINSS - taxIr;
+} else {
+  let taxIr = (salaryLessINSS * 0.275) - 869.36;
+  salaryLiquid = salaryLessINSS - taxIr;
+}
+
+console.log(salaryLiquid);
