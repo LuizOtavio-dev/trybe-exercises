cb61f2e0fb85d75e4128eafc491232eef998b6a8
Luiz Otavio <luiz.otavio273@gmail.com>
2021-03-27 18:59:19 -0300

    Exercicios 4, 5 e 6 - 4 e 5 imprime uma piramide de asteristicos (auxilio do gabarito da Trybe), 6 descobre se um determinado numero e primo

--- 4.3/introducao-a-javascript-e-logica-de-programacao-4.3/script.js
+++ 4.3/introducao-a-javascript-e-logica-de-programacao-4.3/script.js
@@ -35,4 +35,66 @@ for (let index = 0; index < number; index += 1) {
   console.log(asteris);
 }
 
-//4
+//4 (Gabarito Trybe 4.3)
+
+number = 5;
+asteris = '*';
+line = '';
+let position = (number + 1) / 2;
+let left = position;
+let right = position;
+
+for (let index = 0; index < position; index += 1) {
+  for (let counter = 0; counter <= number; counter += 1) {
+    if (counter <= right && counter >= left) {
+      line = line + asteris;
+    } else {
+      line = line + ' ';
+    }    
+  }
+  console.log(line);
+  line = '';
+  right += 1;
+  left -= 1;
+}
+
+//Bônus
+//5 (Gabatiro Trybe 4.3)
+
+number = 5;
+asteris = '*';
+line = '';
+let position = (number + 1) / 2;
+let left = position;
+let right = position;
+
+for (let index = 1; index <= position; index += 1) {
+  for (let counter = 1; counter <= number; counter += 1) {
+    if (counter === right || counter === left || index === position) {
+      line = line + asteris;
+    } else {
+      line = line + ' ';
+    }    
+  }
+  console.log(line);
+  line = '';
+  right += 1;
+  left -= 1;
+}
+
+//6
+
+number = 31;
+let primoNumber = 1;
+
+for (let index = 2; index <= number; index += 1) { 
+  if (number % index === 0) {
+    primoNumber += 1;
+  }
+}
+
+if (primoNumber === 2) {
+  console.log(number + ' é um número primo');
+} else {
+  console.log(number + ' não é um número primo');
+}
