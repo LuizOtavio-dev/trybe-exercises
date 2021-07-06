3b89e273c15fa90771663e4736a60018557fd341
Luiz Otavio <luiz.otavio273@gmail.com>
2021-07-06 20:38:20 -0300

    cria input endereco e funcao generica

--- front-end/bloco-12-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/Form.js
+++ front-end/bloco-12-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/Form.js
@@ -4,27 +4,45 @@ class Form extends Component {
   constructor() {
     super();
     this.state = {
-      name: ''
+      name: '',
+      email: '',
+      cpf: '',
+      address: '',
     }
   }
 
-  handleUpperCase = ({ target: { value } }) => {
-    this.setState({name: value.toUpperCase()})
+  handleState = ({ target: { value, name } }) => {
+    
+    if ( name === 'name') {
+      value = value.toUpperCase()
+      this.setState({ name: value })
+    }
+
+    if ( name === 'address') {
+      value = value.replace(/[^\w\s]/gi, '')
+      this.setState({ address: value })
+    }
+
+    this.setState({ [name]: value })
   }
 
   render() {
-    const { name } = this.state;
+    const { name, address } = this.state;
     return (
       <>
         <fieldset>
           <label htmlFor="name"> Nome
-            <input id="name" type="text" maxLength="40" value={ name } onChange={ this.handleUpperCase } required />
+            <input id="name" type="text" name="name" maxLength="40" value={ name } onChange={ this.handleState } required />
           </label>
           <label htmlFor="email"> Email
-            <input id="email" type="text" maxLength="50" required />
+            <input id="email" type="text" name="email" maxLength="50" onChange={ this.handleState } required />
           </label>
           <label htmlFor="cpf"> CPF
-            <input id="cpf" type="text" maxLength="11" required />
+            <input id="cpf" type="text" name="cpf" maxLength="11" onChange={ this.handleState } required />
+          </label>
+          <br />
+          <label htmlFor="address"> Endereço
+            <input id="address" type="text" name="address" maxLength="200" value={ address } onChange={ this.handleState } required />
           </label>
       </fieldset>
       </>
